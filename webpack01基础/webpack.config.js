76fc68e5c70d7b57eaf1d9836f89dbed2c72a1a1
dep <1649743146@qq.com>
2023-07-16 16:21:30 +0800

    feat: 新增模式配置，以及在开发模式下追踪代码

--- webpack01基础/webpack.config.js
+++ webpack01基础/webpack.config.js
@@ -5,11 +5,22 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         // 输出文件夹必须定义为绝对路径
-        path: path.resolve(__dirname, './dist')
+        path: path.resolve(__dirname, './dist'),
+        // 打包前清理 dist 文件夹
+        clean: true
+
     },
     plugins: [
         // 实例化 html-webpack-plugin 插件
-        new HtmlWebpackPlugin()
+        new HtmlWebpackPlugin({
+            template: './index.html', // 打包生成的文件的模板
+            filename: 'app.html', // 打包生成的文件名称。默认为index.html
+            // 也就是<script src="bundle.js"></script>的位置
+            inject: 'body' // true|'head'|'body'|false，默认值为 true
+        })
     ],
-    mode: 'none'
+    // 开发模式
+    mode: 'development',
+    // 在开发模式下追踪代码
+    devtool: 'inline-source-map'
 }
