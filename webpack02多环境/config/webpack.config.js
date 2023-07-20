8c2b25c151c20ee72469a1cbe806d649b3de1fce
dep <1649743146@qq.com>
2023-07-20 10:54:01 +0800

    feat: 合并配置文件

---
+++ webpack02多环境/config/webpack.config.js
@@ -0,0 +1,12 @@
+const { merge } = require('webpack-merge')
+const commonConfig = require('./webpack.config.common')
+const developmentConfig = require('./webpack.config.dev')
+const productionConfig = require('./webpack.config.prod')
+
+module.exports = (env) => {
+    switch(true) {
+        case env.development: return merge(commonConfig, developmentConfig)
+        case env.production: return merge(commonConfig, productionConfig)
+        default: throw new Error('配置文件错误')
+    }
+}
